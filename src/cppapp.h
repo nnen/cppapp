b28bd4935c5c5f82fffd0a85592fc022f9fcd2c0
Jan Milik <milikjan@fit.cvut.cz>
2012-10-05 14:48:05 +0200

    Add main header file for the library.

---
+++ src/cppapp.h
@@ -0,0 +1,21 @@
+/**
+ * \file   cppapp.h
+ * \author Jan Milik <milikjan@fit.cvut.cz>
+ * \date   2012-10-05
+ *
+ * \brief Header file for the cppapp library.
+ */
+
+#ifndef CPPAPP_XKJCG2SU
+#define CPPAPP_XKJCG2SU
+
+#include "utils.h"
+#include "Object.h"
+#include "Input.h"
+#include "Output.h"
+#include "Options.h"
+#include "AppBase.h"
+#include "Stopwatch.h"
+
+#endif /* end of include guard: CPPAPP_XKJCG2SU */
+
