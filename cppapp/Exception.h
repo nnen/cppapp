0be8dcdcf342f5ab66d9f8fc3dca6dfb0b08c69e
Jan Milík <milikjan@fit.cvut.cz>
2013-06-15 17:08:41 +0200

    Add some documentation to the Error and Exception classes.

--- cppapp/Exception.h
+++ cppapp/Exception.h
@@ -24,18 +24,46 @@ namespace cppapp {
 #define HANDLE_SYSERR(code) { Error(code).exit(); }
 
 
+/**
+ * \brief Represents a unix system error code.
+ */
 struct Error {
+	/// The value system error code.
 	int code;
 	
+	/**
+	 * \brief Constructor.
+	 *
+	 * \param code the system error code
+	 */
 	Error(int code) : code(code) {}
 	
+	/**
+	 * \brief Return \c true if the error code is an actual error, return
+	 *        false otherwise.
+	 */
 	bool isError() const { return code != ERROR_NONE; }
 	
+	/**
+	 * \brief Return the error code message as returned by the \c strerror
+	 *        stdlib function.
+	 */
 	char* getMessage() const { return strerror(code); }
 	
+	/**
+	 * \brief If the code is an error, logs the error and exits the program.
+	 *
+	 * \param msg A message to be logged before the system error code and message.
+	 */
 	void exit(const char *msg);
+	/**
+	 * \brief If the code is an error, logs the error and exits the program.
+	 */
 	void exit();
 	
+	/**
+	 * \brief Convert the Error instance to its error code value.
+	 */
 	operator int() const { return code; }
 };
 
@@ -44,18 +72,18 @@ ostream& operator<<(ostream &output, const Error &error);
 
 
 /**
- * \todo Write documentation for class Exception.
+ * \brief Base class for cppapp/user exceptions.
  */
 class Exception : public std::exception {
 private:
 	/**
-	 * Copy constructor.
+	 * \brief Copy constructor.
 	 */
 	Exception(const Exception& other);
 	
 public:
 	/**
-	 * Constructor.
+	 * \brief Constructor.
 	 */
 	Exception();
 };
